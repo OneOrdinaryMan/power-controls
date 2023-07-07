e9d1eb2c86e9ed38cd8b8ca9390eb6fe75b74dad
One_Ordinary_Man <srivathsansudarsanan@outlook.com>
2023-07-07 15:42:02 +0530

    Added the shutdown script and made the makefile

--- src/script.bash
+++ src/script.bash
@@ -1,2 +1,20 @@
 #!/usr/bin/bash
-echo "Hello, World!"
+rofi_choices="poweroff\nreboot\nlog_out\nsuspend\nsleeep"
+choice=$(echo -e "$rofi_choices" | rofi -dmenu -i -p "Execute:")
+
+case $choice in
+    poweroff)
+        poweroff
+        ;;
+    reboot)
+        reboot
+        ;;
+    log_out)
+        awesome-client 'awesome.quit()'
+        ;;
+    suspend)
+        systemctl suspend
+        ;;
+    sleep)
+        systemctl hybrid-sleep
+esac
